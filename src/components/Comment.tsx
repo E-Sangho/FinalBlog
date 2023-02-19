33957221ee4d7b4e3cecfa13a5b1527e9a7797d2
Sangho <istrangeho@gmail.com>
2023-02-19 22:42:53 +0900

    create comment component

---
+++ src/components/Comment.tsx
@@ -0,0 +1,39 @@
+interface CommentProps {
+	isReversed: boolean;
+}
+
+export default function Comment({ isReversed }: CommentProps) {
+	return (
+		<div className="pl-4 py-4 ">
+			<div className="flex items-center">
+				<div className="rounded-full bg-gray-300 w-8 h-8 text-gray-200 px-2 py-2 mr-4">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 448 512"
+						fill="currentColor"
+						width="100%"
+						height="100%"
+					>
+						<path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
+					</svg>
+				</div>
+				<div className="mr-4">사용자 이름</div>
+				<div className="text-sm text-gray-400">2023-02-19</div>
+			</div>
+			<div className="pl-12">
+				<div className="w-full h-24 rounded-2xl bg-gray-100 mb-2"></div>
+				<div className="flex flex-row-reverse gap-4">
+					<button className="px-4 py-1 bg-gray-300 rounded-2xl text-gray-50">
+						댓글 달기
+					</button>
+					<button className="px-4 py-1 bg-gray-300 rounded-2xl text-gray-50">
+						수정
+					</button>
+					<button className="px-4 py-1 bg-gray-300 rounded-2xl text-gray-50">
+						삭제
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,17 +1,11 @@
+import Comment from "@/components/Comment";
 import Layout from "@/components/layout";
 import MarkdownRenderer from "@/components/markdownRenderer";
 import { useEffect } from "react";
 import { sample } from "../../util/mdSample";
 
 export default function ReadPost() {
-	const textSample = `\`\`\`javascript
-	
-	let a = 0;
-	let b = 0;
-	for (int i = 0; i < 10; ++i) {
-		console.log("hello");
-	}
-	\`\`\``;
+	const textSample = `\`\`\`javascript\nlet a = 0;\nlet b = 0;\nfor (int i = 0; i < 10; ++i) {\n	console.log("hello");\n}\n\`\`\``;
 	const title = "Post Title";
 	const postImage =
 		"https://w.namu.la/s/95f3898eb4996f6ba5a3930b212b295da56e062e9427da87331a510d3d868bd81f24d10d242ca0d93f4ad94053b9321549cb4590ea815a8d39ba92cde1a7da445f694cd13513124c3f6d61e456014a1e0d9a3b6cbe7a28b94c757fbd60bce446";
@@ -40,6 +34,35 @@ export default function ReadPost() {
 			</div>
 			<div className="mx-16 my-32">
 				<MarkdownRenderer text={textSample} />
+			</div>
+			<form className="mx-16 bg-gray-100 rounded-2xl">
+				<div className="px-4 py-4">
+					<div className="flex items-center">
+						<div className="rounded-full bg-gray-300 w-8 h-8 text-gray-200 px-2 py-2 mr-4">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 448 512"
+								fill="currentColor"
+								width="100%"
+								height="100%"
+							>
+								<path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
+							</svg>
+						</div>
+						<div className="text-gray-400">로그인 후 작성할 수 있습니다.</div>
+					</div>
+					<div className="w-full h-24"></div>
+				</div>
+				<div className="flex flex-row-reverse px-4 py-4">
+					<button className="px-4 py-1 bg-gray-300 rounded-2xl text-gray-50">
+						등록
+					</button>
+				</div>
+			</form>
+			<div className="mx-16">
+				{[1, 1, 1, 1].map(() => (
+					<Comment isReversed={false} />
+				))}
 			</div>
 		</Layout>
 	);
