4e002cd2708daa33354a92a8b54eab634f45ca47
Sangho <istrangeho@gmail.com>
2023-03-12 21:03:04 +0900

    update temporaty

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
 		<div className="w-full h-16 bg-blue-500 text-slate-100 flex justify-center items-center opacity-70">
-			&copy; 2023 E-Sangho Debuggint Den
+			&copy; 2023 E-Sangho's Debugging Den
 		</div>
 	);
 }
